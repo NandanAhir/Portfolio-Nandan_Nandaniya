581aea901e0e0e65aa53930d3246cbefd26a2add
Nandan B. Nandaniya <110043314+NandanAhir@users.noreply.github.com>
2023-01-19 08:57:18 -0800

    Add files via upload

---
+++ form.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<center>
+		<?php
+		$conn = mysqli_connect("localhost","root","","portfolio");
+		if($conn === false){
+			die("ERROR::". mysqli_connect_error());
+		}
+		$name = $_REQUEST['name'];
+		$email = $_REQUEST['email'];
+		$message = $_REQUEST['message'];
+
+		$sql = "INSERT INTO form VALUES ('$name', '$email', '$message')";
+		if($query = mysqli_query($conn,$sql)){
+
+            }else{
+            	echo "ERROR: Hush! Sorry $sql. "
+                . mysqli_error($conn);
+            }
+
+		mysqli_close($conn);
+
+		?>
+	</center>
+</body>
+</html>
